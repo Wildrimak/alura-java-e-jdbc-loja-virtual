a5213b3fbb5ce1f78211cdaed6fffaef0fcca9c1
Wildrimak de Souza Pereira <wildrimak@infoway-pi.com.br>
2018-10-26 12:18:25 -0300

    Added TestaConection e TestaListagem

---
+++ src/br/com/caelum/jdbc/TestaListagem.java
@@ -0,0 +1,31 @@
+package br.com.caelum.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaListagem {
+
+	public static void main(String[] args) throws SQLException {
+		Connection connection = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/loja-virtual", "SA", "");
+		Statement statement = connection.createStatement();
+		boolean resultado = statement.execute("Select * from Produto");
+		ResultSet resultSet = statement.getResultSet();
+
+		while (resultSet.next() && resultado) {
+
+			int id = resultSet.getInt("id");
+			String nome = resultSet.getString("nome");
+			String descricao = resultSet.getString("descricao");
+
+			System.out.println(id + ": " + nome + ": " + descricao);
+		}
+
+		resultSet.close();
+		statement.close();
+		connection.close();
+	}
+
+}
